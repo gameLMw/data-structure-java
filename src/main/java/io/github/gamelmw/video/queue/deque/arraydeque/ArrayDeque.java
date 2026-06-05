5b38aecded679049752ac221e8dd59396cb6204c
LM <wycalxy@outlook.com>
2026-06-05 22:32:52 +0800

    ``` chore(data-structure): 更新数组双端队列类注释
    - 添加类功能描述注释
    - 标记为未实现状态的FIXME注释
    - 修正泛型参数格式错误
    ```

--- src/main/java/io/github/gamelmw/video/queue/deque/arraydeque/ArrayDeque.java
+++ src/main/java/io/github/gamelmw/video/queue/deque/arraydeque/ArrayDeque.java
@@ -4,7 +4,12 @@ import io.github.gamelmw.video.queue.deque.Deque;
 
 import java.util.Iterator;
 
-public class ArrayDeque< E>  implements Deque<E>, Iterable<E>{
+/*
+ * 数组表实现双端队列
+ * FIXME：未实现
+ * */
+
+public class ArrayDeque<E> implements Deque<E>, Iterable<E> {
     @Override
     public boolean offerFirst(E e) {
         return false;
