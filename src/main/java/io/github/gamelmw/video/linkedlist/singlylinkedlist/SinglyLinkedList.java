838b1154a8f20e7405eeee205d98c471366b714c
LM <wycalxy@outlook.com>
2026-09-12 18:06:49 +0800

    ``` refactor(SinglyLinkedList): 优化单向链表实现并添加详细注释
    - 将head节点标记为final以提高安全性
    - 添加类级别的注释说明单向链表特性
    - 为addFirst方法添加头插相关注释
    - 为loop1方法添加while循环和函数式接口注释
    - 为loop2方法添加增强for循环注释
    - 使用简化语法创建迭代器实现
    - 为loop3方法添加递归循环注释
    - 为addLast方法添加尾插操作注释
    - 重写findLast方法使用while循环并添加注释
    - 为findNode方法添加查找指定位置节点的注释
    - 为get方法添加获取指定位置元素的注释
    - 为illegalIndex方法添加索引越界异常注释
    - 为insert方法添加插入操作的参数注释
    - 为removeFirst和remove方法添加删除操作注释
    ```

--- src/main/java/io/github/gamelmw/video/linkedlist/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/io/github/gamelmw/video/linkedlist/singlylinkedlist/SinglyLinkedList.java
@@ -3,8 +3,13 @@ package io.github.gamelmw.video.linkedlist.singlylinkedlist;
 import java.util.Iterator;
 import java.util.function.Consumer;
 
+/*
+ * 单项链表
+ * 特性：链表的每个元素都称为节点，每个节点包含数据域和指向下一个节点的指针。
+ * */
+
 public class SinglyLinkedList implements Iterable<Integer> {
-    private Node head = new Node(666, null);
+    private final Node head = new Node(666, null);
 
     private static class Node {
         int value;
@@ -16,10 +21,15 @@ public class SinglyLinkedList implements Iterable<Integer> {
         }
     }
 
+    // 头插
+    // value: 插入的值
     public void addFirst(int value) {
         insert(0, value);
     }
 
+    // 循环1
+    // while循环
+    // 函数式接口（只有1个抽象方法的接口）
     public void loop1(Consumer<Integer> consumer) {
         Node p = head.next;
         while (p != null) {
@@ -28,15 +38,19 @@ public class SinglyLinkedList implements Iterable<Integer> {
         }
     }
 
+    // 循环2
+    // 增强for循环
     public void loop2(Consumer<Integer> consumer) {
         for (Node p = head.next; p != null; p = p.next) {
             consumer.accept(p.value);
         }
     }
 
+    // 循环3
+    // 迭代器
     @Override
     public Iterator<Integer> iterator() {
-        return new Iterator<Integer>() {
+        return new Iterator<>() {
             Node p = head.next;
 
             @Override
@@ -53,10 +67,13 @@ public class SinglyLinkedList implements Iterable<Integer> {
         };
     }
 
+    // 循环4
+    // 递归
     public void loop3() {
         recursion(head.next);
     }
 
+    // 递归
     private void recursion(Node curr) {
         if (curr == null) {
             return;
@@ -66,20 +83,25 @@ public class SinglyLinkedList implements Iterable<Integer> {
         System.out.println("after:" + curr.value);
     }
 
-    private Node findLast() {
-        Node p;
-        for (p = head; p.next != null; p = p.next) {
-
-        }
-        return p;
-    }
-
+    // 尾插
+    // value: 插入的值
     public void addLast(int value) {
         Node last = findLast();
         last.next = new Node(value, null);
     }
 
-    public Node findNode(int index) {
+    // 查找最后一个节点
+    private Node findLast() {
+        Node p = head;
+        while (p.next != null) {
+            p = p.next;
+        }
+        return p;
+    }
+
+    // 查找指定位置的节点
+    // index: 索引
+    private Node findNode(int index) {
         int i = -1;
         for (Node p = head; p != null; p = p.next, i++) {
             if (i == index) {
@@ -89,6 +111,8 @@ public class SinglyLinkedList implements Iterable<Integer> {
         return null;
     }
 
+    // 获取指定位置的元素
+    // index: 索引
     public int get(int index) {
         Node node = findNode(index);
         if (node == null) {
@@ -97,12 +121,16 @@ public class SinglyLinkedList implements Iterable<Integer> {
         return node.value;
     }
 
+    // 索引越界异常
     private static IndexOutOfBoundsException illegalIndex(int index) {
         return new IndexOutOfBoundsException(
                 String.format("index [%d] not valid", index)
         );
     }
 
+    // 插入
+    // index: 索引
+    // value: 插入的值
     public void insert(int index, int value) {
         Node prev = findNode(index - 1);
         if (prev == null) {
@@ -111,10 +139,13 @@ public class SinglyLinkedList implements Iterable<Integer> {
         prev.next = new Node(value, prev.next);
     }
 
+    // 删除头节点
     public void removeFirst() {
         remove(0);
     }
 
+    // 删除
+    // index: 索引
     public void remove(int index) {
         Node prev = findNode(index - 1);
         if (prev == null) {
